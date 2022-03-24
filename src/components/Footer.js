16645eff050bf601baee881dabad25715a78868f
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-03-24 18:47:09 +0200

    korjattu nimeämisiä

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer class="container-fluid py-1 mt-auto">
-            <div class="d-flex justify-content-center p-1">
-                <p class="m-0 fw-bold text-white text-center"></p>
+        <footer className="container-fluid py-1 mt-auto">
+            <div className="d-flex justify-content-center p-1">
+                <p className="m-0 fw-bold text-white text-center"></p>
             </div>
         </footer>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,34 +3,34 @@ import { Link } from 'react-router-dom'; //For Routing: <Link></Link> instead of
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark">
 
-      <Link class="navbar-brand" id='text' to="/">Kiekkokulma</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <Link className="navbar-brand" id='text' to="/">Kiekkokulma</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" id='text' href="#">Tarjoukset</a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" id='text' href="#">Tarjoukset</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Tuoteryhmät
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Kiekot</a>
-              <a class="dropdown-item" href="#">Reput</a>
-              <a class="dropdown-item" href="#">Korit</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Kiekot</a>
+              <a className="dropdown-item" href="#">Reput</a>
+              <a className="dropdown-item" href="#">Korit</a>
             </div>
           </li>
-          <li class="nav-item">
-            <a class="nav-link"  id='text'  href="#">Kirjaudu sisään</a>
+          <li className="nav-item">
+            <a className="nav-link"  id='text'  href="#">Kirjaudu sisään</a>
           </li>
-          <form class="form-inline my-2 my-lg-0">
+          <form className="form-inline my-2 my-lg-0">
             <input
-              class="form-control mr-sm-2" id='searchbar' type="search" placeholder="Search" aria-label="Search" /><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></form>
+              className="form-control mr-sm-2" id='searchbar' type="search" placeholder="Search" aria-label="Search" /><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></form>
         </ul>
       </div>
 
