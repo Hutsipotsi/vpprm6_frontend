74fc44d0434b69fd7ba02e4a3056c659f5d5a598
Annala <n1anje00@students.oamk.fi>
2022-03-24 19:33:21 +0200

    Added carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 export default function Home() {
   return (
-    
+    <div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  </div>
+
     <section class="product-display">
       <div class="product-thumbnail"></div>
       <div class="product-thumbnail"></div>
@@ -11,5 +38,6 @@ export default function Home() {
       <div class="product-thumbnail"></div>
       <div class="product-thumbnail"></div>
     </section>
+    </div>
   )
 }
