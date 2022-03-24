373696ffb636c6038b81eea0a6988c4e245c9ed2
Annala <n1anje00@students.oamk.fi>
2022-03-24 18:41:58 +0200

    Changed text, nav and footer color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark">
 
-      <Link class="navbar-brand" to="/">Kiekkokulma</Link>
+      <Link class="navbar-brand" id='text' to="/">Kiekkokulma</Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,7 +13,7 @@ export default function Navbar() {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Tarjoukset</a>
+            <a class="nav-link" id='text' href="#">Tarjoukset</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -26,11 +26,11 @@ export default function Navbar() {
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Kirjaudu sisään</a>
+            <a class="nav-link"  id='text'  href="#">Kirjaudu sisään</a>
           </li>
           <form class="form-inline my-2 my-lg-0">
             <input
-              class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" /><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></form>
+              class="form-control mr-sm-2" id='searchbar' type="search" placeholder="Search" aria-label="Search" /><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></form>
         </ul>
       </div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
+    
     <section class="product-display">
       <div class="product-thumbnail"></div>
       <div class="product-thumbnail"></div>
