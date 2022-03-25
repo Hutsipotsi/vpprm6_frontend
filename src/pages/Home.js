1cceb74f77966da5e09f5334c6b7917a553302b4
Annala <n1anje00@students.oamk.fi>
2022-03-25 16:16:13 +0200

    Moved picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import firsbeegolf from '../firsbeegolf.jpg';
+import firsbeegolf from '../images/firsbeegolf.jpg';
 
 export default function Home() {
   return (
