a3052fb7012e617e2b4c6447f8a2916eca13554c
Annala <n1anje00@students.oamk.fi>
2022-03-25 16:29:13 +0200

    Added styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar() {
           </li>
           <form className="form-inline my-2 my-lg-0">
             <input
-              className="form-control mr-sm-2" id='searchbar' type="search" placeholder="Search" aria-label="Search" /><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></form>
+              className="form-control mr-sm-2" id='searchbar' type="search" placeholder="Search" aria-label="Search" /><button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button></form>
         </ul>
       </div>
 
