c52ae7372b1646418d9787be2547d27cd9168f62
Annala <n1anje00@students.oamk.fi>
2022-04-06 11:18:42 +0300

    Started updating shopping cart so products amount can be changed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Order from "./components/Order";
+import { editableInputTypes } from "@testing-library/user-event/dist/utils";
 
 const URL = 'http://localhost/vpprm6_backend/';
 
@@ -21,10 +22,19 @@ function App() {
   }, [])
 
 function addToCart(product) {
-  const newCart = [...cart,product];
-  setCart(newCart);
-  localStorage.setItem('cart', JSON.stringify(newCart));
+  if(cart.some(item => item.id === product.id)) {
+    const existingProduct = cart.filter(item => item.id === product.id);
+    updateAmount(parseInt(existingProduct[0].amount) +1,product);
+  
+   }
+  else {
+    product['amount'] = 1
+    const newCart = [...cart,product];
+    setCart(newCart);
+    localStorage.setItem('cart', JSON.stringify(newCart));
+  }
 }
+
 
 function removeFromCart(product) {
   const itemsWithoutRemoved = cart.filter(item => item.id !== product.id);
@@ -32,6 +42,13 @@ function removeFromCart(product) {
   localStorage.setItem('cart', JSON.stringify(itemsWithoutRemoved));
 }
 
+  function updateAmount(amount, product) {
+    product.amount = amount;
+    const index = cart.findIndex((item => item.id === product.id));
+    const modifiedCart = Object.assign([...cart], {[index]: product});
+    setCart(modifiedCart);
+    localStorage.setItem('cart', JSON.stringify(modifiedCart));
+  }
   return (
     <Router>
       {/*<Header /> Header if needed */}
