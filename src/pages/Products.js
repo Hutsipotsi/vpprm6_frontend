f5f4965c84dbf166244f2836f54ddff505b6c57c
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-10 20:51:15 +0300

    fixed products.js code

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,6 +5,9 @@ import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
 import Button from 'react-bootstrap/Button'
 import CloseButton from 'react-bootstrap/CloseButton'
+import { Col, Row } from "react-bootstrap";
+
+            
 
 export default function Products({ url, addToCart }) {
   const [categoryName, setCategoryName] = useState("");
@@ -28,24 +31,29 @@ export default function Products({ url, addToCart }) {
   return (
     <>
       <h3>{categoryName}</h3>
-      <Card border="warning" style={{ width: '18rem' }}>
-        <Card.Body>
+      <container>
+        <Row>
           {products.map((product) => (
-            <div key={product.id}>
-              <Card.Img variant="top" src={url + 'images/' + product.image} alt="tuotekuva" />
-              <Card.Title>{product.name}</Card.Title>
-              <Card.Subtitle className="mb-3">{product.price} €</Card.Subtitle>
-
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Card.Link href={url + 'images/' + product.image}>Iso tuotekuva</Card.Link>
-              <Button variant='btn btn-secondary' id='addCart' type='button' onClick={e => addToCart(product)}>Add to cart</Button>
-            </div>
+            <Col>
+              <Card border="warning" style={{ width: '18rem' }}>
+                <Card.Body>
+                  <div key={product.id}>
+                    <a href={url + 'images/' + product.image}><Card.Img variant="top" src={url + 'images/' + product.image} alt="tuotekuva" /></a>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Subtitle className="mb-3">{product.price} €</Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                    <Card.Link href={url + 'images/' + product.image}>Iso tuotekuva</Card.Link>
+                    <Button variant='btn btn-secondary' id='addCart' type='button' onClick={e => addToCart(product)}>Add to cart</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
           ))}
-          </Card.Body>
-      </Card>
+        </Row>
+      </container>
     </>
   );
 }
