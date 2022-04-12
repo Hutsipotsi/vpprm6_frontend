e18d2d6f01203f687aa678a84f7000c0dcbab897
Iikka Niemikorpi <iikka.niemikorpi@gmail.com>
2022-04-12 20:30:37 +0300

    set height for carousel pictures

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
-import Button from 'react-bootstrap/Button'
-import CloseButton from 'react-bootstrap/CloseButton'
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Button from "react-bootstrap/Button";
+import CloseButton from "react-bootstrap/CloseButton";
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
       <Carousel>
         <Carousel.Item>
           <img
+            id="carouselImage"
             className="d-block w-100"
             img
             src={require("../images/firsbeegolf.jpg")}
@@ -23,6 +24,7 @@ export default function Home() {
         </Carousel.Item>
         <Carousel.Item>
           <img
+            id="carouselImage"
             className="d-block w-100"
             img
             src={require("../images/puppyfrisbee.jpg")}
@@ -36,6 +38,7 @@ export default function Home() {
         </Carousel.Item>
         <Carousel.Item>
           <img
+            id="carouselImage"
             className="d-block w-100"
             img
             src={require("../images/disc-golf.jpg")}
@@ -51,68 +54,143 @@ export default function Home() {
         </Carousel.Item>
       </Carousel>
 
-
       <CardGroup>
-      <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/westside-discs_bt-origio_pohjan-poika.jpg")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/westside-discs_bt-origio_pohjan-poika.jpg")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kiekko Pohjanpoika</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/1/'>Kiekot</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/1/"
+            >
+              Kiekot
+            </Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/backpack.png")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/backpack.png")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kiekko reppu</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/3/'>Reput</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/3/"
+            >
+              Reput
+            </Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/disc-golf.jpg")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/disc-golf.jpg")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kori Target</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/2/'>Korit</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/2/"
+            >
+              Korit
+            </Button>
           </Card.Body>
         </Card>
       </CardGroup>
 
       <CardGroup>
-      <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/disc-golf-bag.jpg")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/disc-golf-bag.jpg")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kiekko kassi</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/3/'>Reput</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/3/"
+            >
+              Reput
+            </Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/innova_champion_krait.jpg")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/innova_champion_krait.jpg")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kiekko Krait</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/1/'>Kiekot</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/1/"
+            >
+              Kiekot
+            </Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={require("../images/disc-golf2.jpg")} alt="PlaceHolderPicture" />
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={require("../images/disc-golf2.jpg")}
+            alt="PlaceHolderPicture"
+          />
           <Card.Body>
             <Card.Title>Kori Traveler</Card.Title>
-            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
-            <Button variant="primary" id='goToProducts' type='button' href='products/2/'>Korit</Button>
+            <Button
+              variant="primary"
+              id="goToProducts"
+              type="button"
+              href="products/2/"
+            >
+              Korit
+            </Button>
           </Card.Body>
         </Card>
       </CardGroup>
-
     </div>
-
   );
 }
