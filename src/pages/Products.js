5e1cc558f44d482df05872962e3a7423cf6b9765
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-18 21:10:07 +0300

    modified page layout

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -55,12 +55,11 @@ export default function Products({ url, addToCart }) {
                 <Card.Body>
                   <div key={product.id}>
                     <a href={url + 'images/' + product.image}><Card.Img variant="top" src={url + 'images/' + product.image} alt="tuotekuva" /></a>
-                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Title className="title">{product.name}</Card.Title>
                     <Card.Subtitle className="mb-3">{product.price} €</Card.Subtitle>
                     <Card.Text> 
                       Some quick example text to build on the card title and make up the bulk of the card's content.
                     </Card.Text>
-                    <Card.Link href={url + 'images/' + product.image}>Iso tuotekuva</Card.Link>
                     <Button variant='btn btn-primary' id='addCart' type='button' onClick={e => addToCart(product)}>Add to cart</Button>
                   </div>
                 </Card.Body>
