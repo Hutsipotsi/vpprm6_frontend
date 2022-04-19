c1ec351669df052e2817e006b0c08ac2dbbf48cd
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-19 19:37:17 +0300

    changed login code to react code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,37 @@
 import React from "react";
+import Form from 'react-bootstrap/Form'
+import { Row, Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 
 const Login = () => {
   return (
-    <form id="loginform">
-      <div class="form-group row">
-        <label for="exampleInputEmail1" class="col-sm-2 col-form-label">Sähköpostiosoite</label>
-        <div class="col-sm-6">
-          <input type="email" class="form-control" id="exampleInputEmail1"
-            aria-describedby="emailHelp" placeholder="Syötä sähköpostisi" />
-          <small id="emailHelp" class="form-text text-muted">
-            Emme koskaan jaa sähköpostiasi kenellekkään.
-          </small>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="exampleInputPassword1" class="col-sm-2 col-form-label">Salasana</label>
-        <div class="col-sm-6">
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Salasana" />
-        </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-6">
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" id="submitButton" class="btn btn-secondary">
-            Lähetä
-          </button>
-        </div>
-      </div>
-    </form>
-  );
-};
-
+    <Form>
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>Sähköposti
+        </Form.Label>
+        <Col sm={6}>
+          <Form.Control type="email" placeholder="Sähköposti" />
+        </Col>
+      </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>Salasana
+          </Form.Label>
+          <Col sm={6}>
+            <Form.Control type="password" placeholder="Salasana" />
+          </Col>
+        </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+            <Col sm={{ span: 6, offset: 2 }}>
+              <Form.Check label="Muista minut" />
+            </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Col sm={{ span: 6, offset: 2 }}>
+                <Button type="submit">Kirjaudu</Button>
+              </Col>
+            </Form.Group>
+      </Form>
+  )
+}
 export default Login;
