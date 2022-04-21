75c38e48d6e1326038e2add6a5d8c370e0106a33
Olli-Pekka <n1haol00@students.oamk.fi>
2022-04-21 17:11:00 +0300

    Fixed onCategoryChange function

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,38 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function CategoryList({url,selectedCategory, setSelectedCategory}) {
+export default function CategoryList({ url, selectedCategory, setSelectedCategory }) {
     const [categories, setCategories] = useState([]);
 
 
-useEffect(() => {
-    axios.get(url + 'products/getproductgroups.php')
-    .then((response) => {
-        const json = response.data;
-        if(json) {
-            if(selectedCategory === null) {
-                setSelectedCategory(json[0]);
-            }
-            setCategories(json);
-        }
+    useEffect(() => {
+        axios.get(url + 'products/getproductgroups.php')
+            .then((response) => {
+                const json = response.data;
+                if (json) {
+                    if (selectedCategory === null) {
+                        setSelectedCategory(json[0]);
+                    }
+                    setCategories(json);
+                }
 
-    }).catch (error => {
-        alert(error.response === undefined ? error : error.response.data.error);
-    })
-}, [selectedCategory])
+            }).catch(error => {
+                alert(error.response === undefined ? error : error.response.data.error);
+            })
+    }, [selectedCategory])
 
-function onCategoryChange(value) {
-    setSelectedCategory(categories.filter(item => item.id === value));
-}
+    function onCategoryChange(value) {
+        let filtered = categories.filter(item => item.id === value);
+        setSelectedCategory(filtered[0]);
+    }
 
 
-return (
-    <select value={selectedCategory?.id} onChange={(e) =>  onCategoryChange(e.target.value)}>
-        {categories.map((category) => (
-            <option key={category.id} value={category.id}>{category.name}</option>
-        ))}
+    return (
+        <select value={selectedCategory?.id} onChange={(e) => onCategoryChange(e.target.value)}>
+            {categories.map((category) => (
+                <option key={category.id} value={category.id}>{category.name}</option>
+            ))}
         </select>
-)
- }
+    )
+}
 
 
